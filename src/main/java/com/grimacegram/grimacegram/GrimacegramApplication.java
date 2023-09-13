9a60907a6dd474663474efbf32e7dd6bd9706cee
asaaev <niaava1@gmail.com>
2023-09-13 13:47:13 +0300

    start project

---
+++ src/main/java/com/grimacegram/grimacegram/GrimacegramApplication.java
@@ -0,0 +1,14 @@
+package com.grimacegram.grimacegram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class GrimacegramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrimacegramApplication.class, args);
+	}
+
+}
